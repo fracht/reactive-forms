64b1f4ad6f77a6cf2b472ddcc8eb3734e5d02d7b
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2020-08-09 22:46:18 +0300

    🐛 Default export bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-export * from './Morfix';
 export * from './MorfixContext';
 export * from './types';
 export * from './useMorfix';
+export * from './Morfix';
+
+import { Morfix } from './Morfix';
+export default Morfix;
