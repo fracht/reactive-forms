f7f3bdbe24b3cb821702a405ca456243d06c6ba6
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2020-08-17 17:37:25 +0300

    ➕ Created simple Morfix Field

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ export * from './types';
 export * from './useMorfix';
 export * from './Morfix';
 export * from './DefaultFieldContext';
+export * from './Field';
 
 import { Morfix } from './Morfix';
 export default Morfix;
