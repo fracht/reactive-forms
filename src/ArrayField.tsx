4467d2c8c7ab02ee42f1207e5ee3dfe29e3a7739
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2020-09-15 20:42:46 +0300

    useArrayField fixes, created ArrayField component

--- src/ArrayField.tsx
+++ src/ArrayField.tsx
@@ -7,6 +7,7 @@ export interface ArrayFieldProps<T> {
     items: Array<T>;
     push: Array<T>['push'];
     pop: Array<T>['pop'];
+    remove: (index: number) => T | undefined;
     set: (item: T, index: number) => void;
     setAll: (items: Array<T>) => void;
 }
@@ -15,7 +16,7 @@ const actionWithCallback = <T extends (...args: unknown[]) => unknown>(
     action: T,
     callback: (output: ReturnType<T>) => void
 ) => (...args: Parameters<T>) => {
-    const output: ReturnType<T> = action(args) as ReturnType<T>;
+    const output: ReturnType<T> = action(...args) as ReturnType<T>;
     callback(output as ReturnType<T>);
     return output;
 };
@@ -31,12 +32,28 @@ export const useArrayField = <T,>({
 
     return {
         items,
-        push: actionWithCallback(items.push, update),
-        pop: actionWithCallback(items.pop, update),
+        push: actionWithCallback(Array.prototype.push.bind(items), update),
+        pop: actionWithCallback(Array.prototype.pop.bind(items), update),
         set: actionWithCallback(
             (item: T, index: number) => (items[index] = item),
             update
         ),
+        remove: (index: number) => {
+            const output = items.splice(index, 1);
+            update();
+            if (output?.length > 0) return output[0];
+            return undefined;
+        },
         setAll
     };
+};
+
+export const ArrayField = <T,>(
+    config: ArrayFieldConfig & {
+        children: (props: ArrayFieldProps<T>) => React.ReactElement;
+    }
+) => {
+    const field = useArrayField<T>(config);
+    const { children } = config;
+    return children(field);
 };
