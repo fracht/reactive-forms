7993685407241b675b51a03b6eee106f30531fe0
sirse <artiom.tretjakovas2@gmail.com>
2020-10-03 23:16:04 +0300

    Deploying project Sat Oct 03 2020 23:15:51 GMT+0300 (Eastern European Summer Time)

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/getting-started":["static\u002Fchunks\u002Fpages\u002Fgetting-started.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgetting-started"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
