7e455bcd31ccb1aba52f140068dd32c6bacd5945
ArtiomTr <44021713+ArtiomTr@users.noreply.github.com>
2020-10-27 19:04:01 +0200

    Set all fields touched after submit (#50)
    * Created setNestedValues utility function
    
    * Setting all fields touched on submit

---
+++ src/utils/setNestedValues.ts
@@ -0,0 +1,26 @@
+import isObject from 'lodash/isObject';
+
+import { NestedObject } from '../typings/NestedObject';
+
+export const setNestedValues = <Example extends object, Value extends object>(
+    exampleObject: Example,
+    value: Value,
+    visited: WeakMap<object, boolean> = new WeakMap(),
+    output: NestedObject<Value, Example> = {} as NestedObject<Value, Example>
+): NestedObject<Value, Example> =>
+    Object.keys(exampleObject).reduce<NestedObject<Value, Example>>((acc, key) => {
+        const part = exampleObject[key];
+
+        if (isObject(part)) {
+            if (!visited.get(part)) {
+                visited.set(part, true);
+                acc[key] = Array.isArray(part) ? [] : {};
+                acc[key] = Object.assign(acc[key], value);
+                setNestedValues(part, value, visited, acc[key]);
+            }
+        } else {
+            acc[key] = value;
+        }
+
+        return acc;
+    }, output);
