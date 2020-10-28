f2c427592039eaeaea8d0b6bca92f2b3e686019c
ArtiomTr <44021713+ArtiomTr@users.noreply.github.com>
2020-10-28 10:28:29 +0200

    Created useArrayField hook (#51)

--- src/hooks/useMorfixError.ts
+++ src/hooks/useMorfixError.ts
@@ -3,8 +3,10 @@ import { useStockState } from 'stocked';
 import { useMorfixContext } from './useMorfixContext';
 import { MorfixErrors } from '../typings';
 
-export const useMorfixError = <V>(name: string): [MorfixErrors<V>, (value: MorfixErrors<V>) => void] => {
+export const useMorfixError = <V>(
+    name: string
+): [MorfixErrors<V> | undefined, (value: MorfixErrors<V> | undefined) => void] => {
     const { errors } = useMorfixContext<object>();
 
-    return useStockState<MorfixErrors<V>>(name, errors);
+    return useStockState<MorfixErrors<V> | undefined>(name, errors);
 };
