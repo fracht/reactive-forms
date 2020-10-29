1fc551282017e7afdc95bffca926dfefbe2482e2
ArtiomTr <44021713+ArtiomTr@users.noreply.github.com>
2020-10-29 14:17:14 +0200

    Created form meta (#54)

---
+++ src/typings/MorfixMeta.ts
@@ -0,0 +1,7 @@
+export type MorfixMeta = {
+    readonly dirty: boolean;
+    readonly isSubmitting: boolean;
+    readonly isValidating: boolean;
+    readonly isValid: boolean;
+    readonly submitCount: number;
+};
