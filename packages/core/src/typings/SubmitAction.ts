3323c291fa9a4ca632e48cc1409397d1b4fa274a
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-06-29 16:39:49 +0300

    [skip ci] Renamed Submit Action's 2nd argument

--- packages/core/src/typings/SubmitAction.ts
+++ packages/core/src/typings/SubmitAction.ts
@@ -1,6 +1,3 @@
 import { MorfixHelpers } from './MorfixHelpers';
 
-export type SubmitAction<Values extends object> = (
-    values: Values,
-    morfixHelpers: MorfixHelpers<Values>
-) => Promise<void>;
+export type SubmitAction<Values extends object> = (values: Values, helpers: MorfixHelpers<Values>) => Promise<void>;
