fbad5b8abbf1b7484c74a550aee067df428700e8
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-07-04 16:05:22 +0300

    Created proxy

--- packages/core/src/components/MorfixContext.ts
+++ packages/core/src/components/MorfixContext.ts
@@ -5,7 +5,3 @@ import { MorfixShared } from '../hooks/useMorfix';
 export type MorfixContextType<Values extends object> = MorfixShared<Values>;
 
 export const MorfixContext = createContext<MorfixContextType<object> | undefined>(undefined);
-
-export const MorfixProvider = MorfixContext.Provider;
-
-export const MorfixConsumer = MorfixContext.Consumer;
--- packages/core/src/hooks/useMorfixControl.ts
+++ packages/core/src/hooks/useMorfixControl.ts
@@ -1,30 +1,16 @@
-import { useCallback } from 'react';
 import { Stock, useStock } from 'stocked';
 
+import { ControlHandlers, useControlHandlers } from './useControlHandlers';
 import { FieldError } from '../typings/FieldError';
 import { FieldTouched } from '../typings/FieldTouched';
 import { FormMeta } from '../typings/FormMeta';
 
 export type MorfixControl<Values extends object> = {
-    setFieldError: <V>(name: string, error: FieldError<V> | undefined) => void;
-    setFieldTouched: <V>(name: string, touched: FieldTouched<V> | undefined) => void;
-    setFieldValue: <V>(name: string, value: V) => void;
-    setFormMeta: <V>(name: keyof FormMeta, value: V) => void;
-
-    getFieldError: <V>(name: string) => FieldError<V> | undefined;
-    getFieldTouched: <V>(name: string) => FieldTouched<V> | undefined;
-    getFieldValue: <V>(name: string) => V | undefined;
-    getFormMeta: <V>(name: keyof FormMeta) => V;
-
-    setErrors: (error: FieldError<Values>) => void;
-    setTouched: (touched: FieldTouched<Values>) => void;
-    setValues: (values: Values) => void;
-
     formMeta: Stock<FormMeta>;
     values: Stock<Values>;
     errors: Stock<FieldError<Values>>;
     touched: Stock<FieldTouched<Values>>;
-};
+} & ControlHandlers<Values>;
 
 export type MorfixControlConfig<Values extends object> = {
     initialValues: Values;
@@ -50,43 +36,10 @@ export const useMorfixControl = <Values extends object>({
     const touched = useStock<FieldTouched<Values>>({ initialValues: initialTouched });
     const formMeta = useStock<FormMeta>({ initialValues: initialFormMeta });
 
-    const setFormMeta = useCallback(
-        (path: keyof FormMeta, value: unknown) => formMeta.setValue(path, value),
-        [formMeta]
-    );
-    const getFormMeta = useCallback(<V>(path: keyof FormMeta) => formMeta.getValue<V>(path), [formMeta]);
-
-    const setFieldValue = useCallback(<V>(name: string, value: V) => values.setValue(name, value), [values]);
-    const getFieldValue = useCallback(<V>(name: string) => values.getValue<V>(name), [values]);
-    const setValues = useCallback((newValues: Values) => values.setValues(newValues), [values]);
-
-    const setFieldTouched = useCallback(
-        <V>(name: string, fieldTouched: FieldTouched<V> | undefined) => touched.setValue(name, fieldTouched),
-        [touched]
-    );
-    const getFieldTouched = useCallback(<V>(name: string) => touched.getValue<FieldTouched<V>>(name), [touched]);
-    const setTouched = useCallback((allTouched: FieldTouched<Values>) => touched.setValues(allTouched), [touched]);
-
-    const setFieldError = useCallback(
-        <V>(name: string, error: FieldError<V> | undefined) => errors.setValue(name, error),
-        [errors]
-    );
-    const getFieldError = useCallback(<V>(name: string) => errors.getValue<FieldError<V>>(name), [errors]);
-    const setErrors = useCallback((allErrors: FieldError<Values>) => errors.setValues(allErrors), [errors]);
+    const handlers = useControlHandlers({ values, errors, touched, formMeta });
 
     return {
-        setFormMeta,
-        setFieldValue,
-        setValues,
-        setFieldTouched,
-        setTouched,
-        setFieldError,
-        setErrors,
-
-        getFieldValue,
-        getFieldTouched,
-        getFieldError,
-        getFormMeta,
+        ...handlers,
 
         formMeta,
         errors,
