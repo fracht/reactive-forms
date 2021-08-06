9c352f413b48d40db335219f1b58c60b18a49f65
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-08-06 14:27:11 +0300

    Created possibility to specifiy initialValues via promise

--- packages/core/src/typings/FormHelpers.ts
+++ packages/core/src/typings/FormHelpers.ts
@@ -1,9 +1,9 @@
 import { FieldError } from './FieldError';
-import { FormResetConfig } from '../hooks/useForm';
+import { InitialFormState } from '../hooks/useForm';
 import { FormControl } from '../hooks/useFormControl';
 import { ValidationRegistryControl } from '../hooks/useValidationRegistry';
 
 export type FormHelpers<Values extends object> = FormControl<Values> & {
     validateForm: (values: Values) => Promise<FieldError<Values>>;
-    resetForm: (config?: FormResetConfig<Values>) => void;
+    resetForm: (state?: InitialFormState<Values>) => void;
 } & Pick<ValidationRegistryControl, 'validateField'>;
