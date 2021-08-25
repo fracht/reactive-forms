7099b386f30c9ff166f82fde3dfa3d04d5aa7669
Aleksandras Sukelovic <alex.shukel@gmail.com>
2021-08-25 18:40:14 +0300

    Added test in deepRemoveEmpty.test.ts

--- packages/core/src/utils/deepRemoveEmpty.ts
+++ packages/core/src/utils/deepRemoveEmpty.ts
@@ -1,19 +1,18 @@
 import isEmpty from 'lodash/isEmpty';
 import isNil from 'lodash/isNil';
 
-import { FieldError } from '../typings/FieldError';
-
-type UnknownArrayErrorType = { $error: FieldError<unknown[]> | undefined } & Array<unknown>;
-
 export const deepRemoveEmpty = (obj: object): object | undefined => {
     if (Array.isArray(obj)) {
-        let error: FieldError<unknown[]> | undefined;
-        if ('$error' in obj) {
-            error = (obj as UnknownArrayErrorType).$error;
-        }
-        const newArr = obj.map((value) => (typeof value === 'object' ? deepRemoveEmpty(value) : value));
-        if (error) (newArr as UnknownArrayErrorType).$error = error;
-        return newArr.every(isNil) && !error ? undefined : newArr;
+        const newArr = Object.assign(
+            [],
+            Object.fromEntries(
+                Object.entries(obj).map(([key, value]) => [
+                    key,
+                    typeof value === 'object' ? deepRemoveEmpty(value) : value
+                ])
+            )
+        );
+        return Object.values(newArr).every(isNil) ? undefined : newArr;
     } else if (obj !== null && typeof obj === 'object') {
         const newObj = Object.keys(obj).reduce((acc, key) => {
             const value = typeof obj[key] === 'object' ? deepRemoveEmpty(obj[key]) : obj[key];
