f00ea588b143ee0c0a0a49dbb79600d29f956d5e
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2021-12-30 13:02:12 +0200

    Started migrating to turbo (#188)
    * Started migrating to turbo
    
    * Created publisher package
    
    * Fixed actions
    
    * Fixed CI pipeline
    
    * Fixed danger(dungeon)file(master)
    
    * Created tools workspace
    
    * Moved tools folder
    
    Co-authored-by: Aleksandras Sukelovic <alex.shukel@gmail.com>

--- dangerfile.js
+++ dangerfile.js
@@ -1,16 +1,4 @@
 const { danger, warn } = require('danger');
-const { match } = require('micromatch');
-
-const modifiedPackageJsons = match(danger.git.modified_files, '**/package.json');
-
-modifiedPackageJsons.forEach((packageJson) => {
-    const lockfile = packageJson.replace('package.json', 'package-lock.json');
-    if (!danger.git.modified_files.includes(lockfile)) {
-        warn(
-            `Changes were made to ${packageJson}, but ${lockfile} still unchanged. If you changed dependencies, please, commit changed lockfile.`
-        );
-    }
-});
 
 var bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
