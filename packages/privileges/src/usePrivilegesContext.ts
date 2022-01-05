c486815bd4b3f8d66fb19ae36356d48df992baa0
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-01-05 14:50:39 +0200

    Fixed invariant message

--- packages/privileges/src/usePrivilegesContext.ts
+++ packages/privileges/src/usePrivilegesContext.ts
@@ -6,7 +6,7 @@ import { PrivilegesContext, PrivilegesContextType } from './PrivilegesContext';
 export const usePrivilegesContext = <Values extends object>() => {
     const context = useContext(PrivilegesContext);
 
-    invariant(context, "You're trying to access PrivilegesContext outside <ReactiveForm> tag");
+    invariant(context, "You're trying to access PrivilegesContext outside PrivilegesContext provider");
 
     return context as unknown as PrivilegesContextType<Values>;
 };
