6bfcad38ab73f929c06cdc0a10d70e2293879b44
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-01-28 17:03:10 +0200

    Bug fix

--- packages/x/src/plugin.ts
+++ packages/x/src/plugin.ts
@@ -1,7 +1,7 @@
-import { FormShared, Plugin } from '@reactive-forms/core';
+import { FormConfig, FormShared, Plugin } from '@reactive-forms/core';
 
 export const xPlugin: Plugin = {
     token: Symbol.for('x'),
-    useConfigDecorator: (config) => config,
+    useConfigDecorator: <T extends object>(config: FormConfig<T>) => config,
     useBagDecorator: <T extends object>(form: FormShared<T>) => form
 };
