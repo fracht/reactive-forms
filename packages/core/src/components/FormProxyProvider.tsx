113fc3d2da0fed169736ff98c06d9b7e17c5a7e5
Aleksandras Šukelovič <64651944+AlexShukel@users.noreply.github.com>
2022-03-23 16:43:40 +0200

    Fixed bug in validation interception (#201)
    * Fixed bug in validation interception
    
    * Fixed test

--- packages/core/src/components/FormProxyProvider.tsx
+++ packages/core/src/components/FormProxyProvider.tsx
@@ -1,11 +1,9 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext } from 'react';
 import { Pxth } from 'pxth';
-import { intercept, StockProxy, useStockContext } from 'stocked';
+import { StockProxy } from 'stocked';
 
 import { FormContext } from './FormContext';
-import { useControlHandlers } from '../hooks/useControlHandlers';
-import { useFormContext } from '../hooks/useFormContext';
-import { FieldValidator } from '../typings/FieldValidator';
+import { useProxyInterception } from '../hooks/useProxyInterception';
 
 export type FormProxyProviderProps<V> = {
     proxy: StockProxy<V>;
@@ -15,42 +13,10 @@ export type FormProxyProviderProps<V> = {
 export const FormProxyContext = createContext<StockProxy<unknown> | undefined>(undefined);
 
 export const FormProxyProvider = <V,>({ proxy, children }: FormProxyProviderProps<V>) => {
-    const { values, errors, touched, formMeta, registerValidator, ...other } = useFormContext();
-
-    const newValues = useStockContext(values, proxy as StockProxy<unknown>);
-    const newErrors = useStockContext(errors, proxy as StockProxy<unknown>);
-    const newTouched = useStockContext(touched, proxy as StockProxy<unknown>);
-
-    const handlers = useControlHandlers({ values: newValues, errors: newErrors, touched: newTouched, formMeta });
-
-    const { getFieldValue } = handlers;
-
-    const interceptedRegisterValidator = useCallback(
-        <V,>(name: Pxth<V>, validator: FieldValidator<V>) =>
-            intercept(
-                proxy as StockProxy<unknown>,
-                name as Pxth<unknown>,
-                registerValidator,
-                (name, validator) => {
-                    return registerValidator(proxy.getNormalPath(name), () => validator(getFieldValue(name)!));
-                },
-                [name as Pxth<unknown>, validator as FieldValidator<unknown>]
-            ),
-        [getFieldValue, proxy, registerValidator]
-    );
+    const newContext = useProxyInterception(proxy);
 
     return (
-        <FormContext.Provider
-            value={{
-                ...other,
-                ...handlers,
-                formMeta,
-                values: newValues,
-                errors: newErrors,
-                touched: newTouched,
-                registerValidator: interceptedRegisterValidator
-            }}
-        >
+        <FormContext.Provider value={newContext}>
             <FormProxyContext.Provider value={proxy as StockProxy<unknown>}>
                 {typeof children === 'function' ? children(proxy.path) : children}
             </FormProxyContext.Provider>
---
+++ packages/core/tests/hooks/useProxyInterception.test.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+import { createPxth, getPxthSegments, Pxth } from 'pxth';
+import { MappingProxy } from 'stocked';
+
+import { FieldInnerError, FieldValidator, ReactiveFormProvider, useForm } from '../../src';
+import { useProxyInterception } from '../../src/hooks/useProxyInterception';
+import { NestedObject } from '../../src/typings/NestedObject';
+
+type ProxyValue = {
+    id: number;
+    person: {
+        address: string;
+        birthDate: Date;
+    };
+    languages: string[];
+};
+
+const renderUseProxyInterception = () => {
+    const { result: bag } = renderHook(() =>
+        useForm({
+            initialValues: {
+                values: {
+                    real: {
+                        person_id: 1,
+                        data: {
+                            p_address: 'hello',
+                            p_birthDate: new Date()
+                        },
+                        learned_languages: ['EN', 'LT']
+                    }
+                }
+            }
+        })
+    );
+
+    const proxy = new MappingProxy<ProxyValue>(
+        {
+            id: createPxth<number>(['values', 'real', 'person_id']),
+            person: {
+                address: createPxth<string>(['values', 'real', 'data', 'p_address']),
+                birthDate: createPxth<Date>(['values', 'real', 'data', 'p_birthDate'])
+            },
+            languages: createPxth<string[]>(['values', 'real', 'learned_languages'])
+        },
+        createPxth(['values', 'proxy'])
+    );
+
+    proxy.activate();
+
+    const registerValidator = jest.fn(bag.current.registerValidator);
+    const validateField = jest.fn(bag.current.validateField);
+
+    const { result } = renderHook(() => useProxyInterception(proxy), {
+        wrapper: ({ children }) => {
+            return (
+                <ReactiveFormProvider
+                    formBag={{
+                        ...bag.current,
+                        registerValidator: registerValidator as <V>(
+                            name: Pxth<V>,
+                            validator: FieldValidator<V>
+                        ) => () => void,
+                        validateField: validateField as <V>(
+                            name: Pxth<V>,
+                            value: V
+                        ) => Promise<NestedObject<FieldInnerError, V>>
+                    }}
+                >
+                    {() => children}
+                </ReactiveFormProvider>
+            );
+        }
+    });
+
+    return { result, registerValidator, validateField };
+};
+
+describe('useProxyInterception', () => {
+    it('registerValidator should register intercepted validator', () => {
+        const { result, registerValidator } = renderUseProxyInterception();
+
+        const proxiedFieldPath = createPxth(['values', 'proxy', 'person']);
+
+        const validator = jest.fn();
+        result.current.registerValidator(proxiedFieldPath, validator);
+
+        expect(getPxthSegments(registerValidator.mock.calls[0][0])).toStrictEqual(['values', 'real', 'data']);
+
+        result.current.validateField(proxiedFieldPath, {
+            address: 'asdf',
+            birthDate: new Date(100)
+        });
+
+        expect(validator.mock.calls[0][0]).toStrictEqual({
+            address: 'asdf',
+            birthDate: new Date(100)
+        });
+    });
+
+    it('intercepted validateField should call real validateField with real values', () => {
+        const { result, validateField } = renderUseProxyInterception();
+
+        const proxiedFieldPath = createPxth(['values', 'proxy', 'person']);
+
+        result.current.validateField(proxiedFieldPath, { address: 'asdf', birthDate: new Date(100) });
+        expect(getPxthSegments(validateField.mock.calls[0][0])).toStrictEqual(['values', 'real', 'data']);
+        expect(validateField.mock.calls[0][1]).toStrictEqual({
+            p_address: 'asdf',
+            p_birthDate: new Date(100)
+        });
+    });
+});
