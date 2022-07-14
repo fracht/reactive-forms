5bd9d912df7870b746a4143f7d996296107358a8
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-07-14 13:34:33 +0300

    Added test for ReactiveForm component

---
+++ packages/core/tests/components/ReactiveForm.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+
+import ReactiveForm, { useFormContext } from '../../src';
+
+describe('ReactiveForm', () => {
+    it('should render children as function', () => {
+        const wrapper = ({ children }) => <ReactiveForm initialValues={{}}>{() => children}</ReactiveForm>;
+
+        const { result } = renderHook(() => useFormContext(), { wrapper });
+
+        expect(result.current).toBeDefined();
+    });
+
+    it('should render children as ReactNode', () => {
+        const wrapper = ({ children }) => <ReactiveForm initialValues={{}}>{children}</ReactiveForm>;
+
+        const { result } = renderHook(() => useFormContext(), { wrapper });
+
+        expect(result.current).toBeDefined();
+    });
+});
