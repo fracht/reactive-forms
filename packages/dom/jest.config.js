f727eb80ad142b66dfdca3c84f69e79b201e0e2e
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-10-23 23:11:19 +0300

    Fixed tests configuration

--- packages/dom/jest.config.js
+++ packages/dom/jest.config.js
@@ -1,12 +1,22 @@
-module.exports = {
-    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|cjs|jsx)$'"],
-    preset: 'ts-jest/presets/js-with-babel',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'cjs', 'mjs', 'json', 'node'],
-    collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx,cjs,mjs}'],
-    testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx,cjs,mjs}'],
-    testEnvironmentOptions: {
-        url: 'http://localhost'
-    },
-    watchPlugins: ['jest-watch-typeahead\\filename.js', 'jest-watch-typeahead\\testname.js'],
-    testEnvironment: 'jsdom'
+/**
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ */
+ const config = {
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|cjs|jsx)$'"],
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'cjs', 'mjs', 'json', 'node'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx,cjs,mjs}'],
+  testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx,cjs,mjs}'],
+  testEnvironmentOptions: {
+      url: 'http://localhost'
+  },
+  watchPlugins: ['jest-watch-typeahead\\filename.js', 'jest-watch-typeahead\\testname.js'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.test.json'
+    }]
+  }
 };
+
+module.exports = config;
