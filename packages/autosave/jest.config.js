eb5a0a9f4bf00c505b77b325f6b6a4bc712a472b
Aleksandras Sukelovic <alex.shukel@gmail.com>
2022-11-04 13:13:22 +0200

    Configured jest in autosave package

---
+++ packages/autosave/jest.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ */
+ const config = {
+	transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|cjs|jsx)$'"],
+	preset: 'ts-jest',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'cjs', 'mjs', 'json', 'node'],
+	collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx,cjs,mjs}'],
+	testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx,cjs,mjs}'],
+	testEnvironmentOptions: {
+		url: 'http://localhost',
+	},
+	testEnvironment: 'jsdom'
+};
+
+module.exports = config;
