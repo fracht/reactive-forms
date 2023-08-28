6fa257ad7bf3dd2f13217dd7c2a2624217ed3003
Aleksandras Sukelovic <alex.shukel@gmail.com>
2023-08-28 15:34:22 +0300

    Created useDecimalField

---
+++ packages/x/src/useDecimalField.ts
@@ -0,0 +1,142 @@
+import { useCallback } from 'react';
+import { FieldConfig, useFieldValidator } from '@reactive-forms/core';
+
+import { ConversionError, ConverterFieldBag, useConverterField } from './useConverterField';
+
+const DECIMAL_REGEX = /^\d*\.?\d*$/;
+
+export const defaultLocales: Intl.LocalesArgument = 'EN';
+
+export const defaultFormatOptions: Intl.NumberFormatOptions = {
+	minimumFractionDigits: 1,
+	maximumFractionDigits: 2,
+};
+
+const formatDecimal = (
+	value: number | null | undefined,
+	locales?: Intl.LocalesArgument,
+	options?: Intl.NumberFormatOptions,
+) => {
+	if (typeof value !== 'number' || !Number.isFinite(value)) {
+		return '';
+	}
+
+	return value.toLocaleString(locales, options);
+};
+
+export type DecimalFieldErrorMessages = {
+	invalidInput: string;
+	required: string;
+	lessThanMinValue: (min: number, locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => string;
+	moreThanMaxValue: (max: number, locales?: Intl.LocalesArgument, options?: Intl.NumberFormatOptions) => string;
+};
+
+export const defaultErrorMessages: DecimalFieldErrorMessages = {
+	invalidInput: 'Must be decimal',
+	required: 'Field is required',
+	lessThanMinValue: (min, locales, options) =>
+		`Value should not be less than ${formatDecimal(min, locales, options)}`,
+	moreThanMaxValue: (max, locales, options) =>
+		`Value should not be more than ${formatDecimal(max, locales, options)}`,
+};
+
+export type DecimalFieldConfig = FieldConfig<number | null | undefined> & {
+	required?: boolean;
+	min?: number;
+	max?: number;
+
+	formatValue?: (value: number | null | undefined) => string;
+	errorMessages?: Partial<DecimalFieldErrorMessages>;
+
+	locales?: Intl.LocalesArgument;
+	formatOptions?: Intl.NumberFormatOptions;
+};
+
+export type DecimalFieldBag = ConverterFieldBag<number | null | undefined> & {};
+
+export const useDecimalField = ({
+	name,
+	validator,
+	schema,
+	required,
+	min,
+	max,
+	formatValue,
+	errorMessages = defaultErrorMessages,
+	locales = defaultLocales,
+	formatOptions = defaultFormatOptions,
+}: DecimalFieldConfig): DecimalFieldBag => {
+	const parseDecimal = useCallback(
+		(text: string) => {
+			text = text.trim();
+
+			if (text.length === 0) {
+				return null;
+			}
+
+			const errorMessage = errorMessages.invalidInput ?? defaultErrorMessages.invalidInput;
+
+			if (!DECIMAL_REGEX.test(text)) {
+				throw new ConversionError(errorMessage);
+			}
+
+			const value = Number.parseFloat(text);
+
+			if (Number.isNaN(value)) {
+				// "." is valid decimal number zero, however Number.parseFloat returns NaN
+				if (text === '.') {
+					return 0;
+				}
+
+				throw new ConversionError(errorMessage);
+			}
+
+			return value;
+		},
+		[errorMessages.invalidInput],
+	);
+
+	const format = useCallback(
+		(value: number | null | undefined) => {
+			if (formatValue) {
+				return formatValue(value);
+			}
+
+			return formatDecimal(value, locales, formatOptions);
+		},
+		[formatOptions, formatValue, locales],
+	);
+
+	const decimalBag = useConverterField({
+		parse: parseDecimal,
+		format,
+		name,
+		validator,
+		schema,
+	});
+
+	useFieldValidator({
+		name,
+		validator: (value) => {
+			if (required && typeof value !== 'number') {
+				return errorMessages.required ?? defaultErrorMessages.required;
+			}
+
+			if (typeof value !== 'number') {
+				return undefined;
+			}
+
+			if (typeof min === 'number' && value < min) {
+				return (errorMessages.lessThanMinValue ?? defaultErrorMessages.lessThanMinValue)(min);
+			}
+
+			if (typeof max === 'number' && value > max) {
+				return (errorMessages.moreThanMaxValue ?? defaultErrorMessages.moreThanMaxValue)(max);
+			}
+
+			return undefined;
+		},
+	});
+
+	return decimalBag;
+};
---
+++ packages/x/tests/useDecimalField.test.tsx
@@ -0,0 +1,312 @@
+import React from 'react';
+import { ReactiveFormProvider, useForm } from '@reactive-forms/core';
+import { act, renderHook, waitFor } from '@testing-library/react';
+
+import {
+	DecimalFieldConfig,
+	defaultErrorMessages,
+	defaultFormatOptions,
+	defaultLocales,
+	useDecimalField,
+} from '../src/useDecimalField';
+
+type Config = Omit<DecimalFieldConfig, 'name'> & {
+	initialValue?: number | null;
+};
+
+const renderUseDecimalField = (config: Config = {}) => {
+	const { initialValue = 0, ...initialProps } = config;
+
+	const formBag = renderHook(() =>
+		useForm({
+			initialValues: {
+				test: initialValue,
+			},
+		}),
+	);
+
+	const decimalFieldBag = renderHook(
+		(props: Omit<DecimalFieldConfig, 'name'>) =>
+			useDecimalField({
+				name: formBag.result.current.paths.test,
+				...props,
+			}),
+		{
+			wrapper: ({ children }) => (
+				<ReactiveFormProvider formBag={formBag.result.current}>{children}</ReactiveFormProvider>
+			),
+			initialProps,
+		},
+	);
+
+	return [decimalFieldBag, formBag] as const;
+};
+
+describe('Decimal field', () => {
+	it('Should format initial value correctly', () => {
+		const [{ result }] = renderUseDecimalField();
+
+		expect(result.current.text).toBe((0).toLocaleString(defaultLocales, defaultFormatOptions));
+		expect(result.current.value).toBe(0);
+	});
+
+	it('Should set default conversion error correctly', async () => {
+		const [{ result }] = renderUseDecimalField();
+
+		await act(() => {
+			result.current.onTextChange('0a');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.invalidInput);
+		});
+
+		await act(() => {
+			result.current.onTextChange('a0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.invalidInput);
+		});
+
+		await act(() => {
+			result.current.onTextChange('hello');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.invalidInput);
+		});
+
+		await act(() => {
+			result.current.onTextChange('0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(null);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('     ');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(null);
+			expect(result.current.text).toBe('     ');
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('.');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('.0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('0.');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+	});
+
+	it('Should set default error if field is required and empty', async () => {
+		const [{ result }] = renderUseDecimalField({ required: true });
+
+		act(() => {
+			result.current.control.setValue(null);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.required);
+		});
+	});
+
+	it('Should set default error if field value is less than min', async () => {
+		const [{ result }] = renderUseDecimalField({ min: 0.5 });
+
+		act(() => {
+			result.current.control.setValue(0.25);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.lessThanMinValue(0.5));
+		});
+	});
+
+	it('Should set default error if field value is more than max', async () => {
+		const [{ result }] = renderUseDecimalField({ max: 0.5 });
+
+		act(() => {
+			result.current.control.setValue(0.75);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe(defaultErrorMessages.moreThanMaxValue(0.5));
+		});
+	});
+
+	it('Should set custom conversion error correctly', async () => {
+		const [{ result }] = renderUseDecimalField({
+			errorMessages: {
+				invalidInput: 'custom',
+			},
+		});
+
+		await act(() => {
+			result.current.onTextChange('0a');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+
+		await act(() => {
+			result.current.onTextChange('a0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+
+		await act(() => {
+			result.current.onTextChange('hello');
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+
+		await act(() => {
+			result.current.onTextChange('0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(null);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('     ');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(null);
+			expect(result.current.text).toBe('     ');
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('.');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('.0');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+
+		await act(() => {
+			result.current.onTextChange('0.');
+		});
+
+		await waitFor(() => {
+			expect(result.current.value).toBe(0);
+			expect(result.current.meta.error?.$error).toBeUndefined();
+		});
+	});
+
+	it('Should set custom error if field is required and empty', async () => {
+		const [{ result }] = renderUseDecimalField({
+			required: true,
+			errorMessages: { required: 'custom' },
+		});
+
+		act(() => {
+			result.current.control.setValue(null);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+	});
+
+	it('Should set custom error if field value is less than min', async () => {
+		const [{ result }] = renderUseDecimalField({
+			min: 0.5,
+			errorMessages: { lessThanMinValue: () => 'custom' },
+		});
+
+		act(() => {
+			result.current.control.setValue(0.25);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+	});
+
+	it('Should set custom error if field value is more than max', async () => {
+		const [{ result }] = renderUseDecimalField({
+			max: 0.5,
+			errorMessages: { moreThanMaxValue: () => 'custom' },
+		});
+
+		act(() => {
+			result.current.control.setValue(0.75);
+		});
+
+		await waitFor(() => {
+			expect(result.current.meta.error?.$error).toBe('custom');
+		});
+	});
+
+	it('Should be able to format decimal differently', () => {
+		const formatValue = jest.fn(() => 'custom');
+		const initialValue = 3.14;
+		const [{ result }] = renderUseDecimalField({ formatValue, initialValue });
+
+		expect(result.current.text).toBe('custom');
+		expect(formatValue).toBeCalledWith(initialValue);
+	});
+});
